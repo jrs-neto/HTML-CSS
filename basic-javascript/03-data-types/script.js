4bef91e934a21cb78ef56e297bb71a8793704cdc
jrs-neto <jrd.santosneto@gmail.com>
2026-01-18 10:03:01 -0300

    Study: practice JavaScript data types and operators

---
+++ basic-javascript/03-data-types/script.js
@@ -0,0 +1,20 @@
+const text = "Javacript";
+const frase = 'Isso é o meu "jogo"';
+
+let total = 0;
+
+const compras = 30;
+const imposto = 10;
+
+total = compras + imposto;
+
+const dobro = total * 2;
+const dividir = total / 2;
+const desconto = total - 30;
+
+const strings = "20" + "20"; // Concatena os valores
+const numbers = 20 + 20; // soma os valores
+
+const transformar = Number("20") + 20; // Converte string em número
+
+console.log(transformar);
