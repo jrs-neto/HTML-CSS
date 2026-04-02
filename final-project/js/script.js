cd6b5f1ea28e157c5142780bf5bbcf892b0d0fbb
jrs-neto <jrd.santosneto@gmail.com>
2026-04-02 11:15:03 -0300

    feat: auto-select budget items from URL parameters

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
